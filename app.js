2031d9bb9e3cf9c7454ca3164f11f963b16d258d
surabhi0_0 <bishwassurabhi0@gmail.com>
2025-12-30 19:58:00 +0530

    Update app.js to change heading text, add console logs, and render JSX element instead of React.createElement. Comment out the previous render method.

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const element = React.createElement('h1', {id: 'headingId', xyz: 'abc'}, 'Hello World from Surabhi');
+const element = React.createElement('h1', {id: 'headingId', xyz: 'abc'}, 'Hello World from Test');
+console.log(element);
 const rootElement = ReactDOM.createRoot(document.getElementById('root'));
-rootElement.render(element);
+console.log(typeof rootElement);
+
+// JSX is not HTML in JavaScript, it is HTML like syntax in JavaScript.
+const jsXElement = <h1>Hello World from JSX</h1>;
+console.log(jsXElement);
+// rootElement.render(element);
+rootElement.render(jsXElement);
