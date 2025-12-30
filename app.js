d20a83f17643ded027dcad67b24b1f97395eb8a5
surabhi0_0 <bishwassurabhi0@gmail.com>
2025-12-30 18:23:03 +0530

    Refactor app.js to use ES6 imports for React and ReactDOM, and update the heading text. Modify index.html to load app.js as a module and comment out legacy script tags for React.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const element = React.createElement('h1', {id: 'headingId', xyz: 'abc'}, 'Hello World from JavaScript');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const element = React.createElement('h1', {id: 'headingId', xyz: 'abc'}, 'Hello World from Surabhi');
 const rootElement = ReactDOM.createRoot(document.getElementById('root'));
 rootElement.render(element);
