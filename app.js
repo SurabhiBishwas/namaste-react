a57452dc2bfb2349a88244807f20bb6d5eba0679
surabhi0_0 <bishwassurabhi0@gmail.com>
2026-01-01 16:16:54 +0530

    Implement HomeComponent with NavComponent and FooterComponent in app.js, enhancing the structure of the application. Update the render method to display the new HomeComponent.

--- app.js
+++ app.js
@@ -15,8 +15,44 @@ const jsXElement = (<div>
     <h2>Hello World from Neha</h2>
 </div>);
 
+
+const NavComponent = () => {
+    return (
+        <div style= {{
+            display: 'flex',
+            gap: '10px'
+        }}>
+            <span>Home</span>
+            <span>About</span>
+            <span>Contact</span>
+            <span>Cart</span>
+        </div>
+    )
+};
+
+const ContactComponent = () => <h1>Contact Component</h1>;
+
+const FooterComponent = () => (
+    <div>
+        <ContactComponent/>
+        <h1>Footer Component</h1>
+    </div>
+);
+
+// component composition is the process of combining multiple components to create a new component.
+
+function HomeComponent() {
+    return (
+        <div>
+            <NavComponent />
+            <h1>Home Component</h1>
+            <FooterComponent/>
+        </div>
+    )
+}
+
 console.log(typeof jsXElement);
 
 console.log(jsXElement);
 // rootElement.render(element);
-rootElement.render(jsXElement);
+rootElement.render(<HomeComponent/>); // this is a JSX element, not a ReactElement.
