5587d98443f57d23f0f50dd7046aef6b5d17ec7c
surabhi0_0 <bishwassurabhi0@gmail.com>
2026-01-01 15:16:11 +0530

    Enhance app.js with detailed comments on JSX transpilation, update JSX structure to include nested elements, and log the type of the JSX element.

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ import ReactDOM from 'react-dom/client';
 const element = React.createElement('h1', {id: 'headingId', xyz: 'abc'}, 'Hello World from Test');
 console.log(element);
 const rootElement = ReactDOM.createRoot(document.getElementById('root'));
-console.log(typeof rootElement);
 
 // JSX is not HTML in JavaScript, it is HTML like syntax in JavaScript.
-const jsXElement = <h1>Hello World from JSX</h1>;
+// JSX is transpiled/converted/compiled to JavaScript by Babel. JSX is not a valid JavaScript syntax. Parccel has Babel installed by default.
+
+// JSX => Babel => React.createElement() => JS object (ReactElement) => ReactDOM.render() => HTML
+
+const jsXElement = (<div>
+    Hello World from Surabhi
+    <h2>Hello World from Neha</h2>
+</div>);
+
+console.log(typeof jsXElement);
+
 console.log(jsXElement);
 // rootElement.render(element);
 rootElement.render(jsXElement);
