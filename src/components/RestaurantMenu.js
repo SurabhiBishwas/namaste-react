7e5a1515420276024bbc5f9f698ca91082b7b64b
surabhi0_0 <bishwassurabhi0@gmail.com>
2026-01-24 15:43:30 +0530

    restaurant menu card

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,25 +1,20 @@
 import Shimmer from './Shimmer';
 import { useParams } from 'react-router-dom';
 import useRestaurantMenu from '../utils/useRestaurantMenu';
+import RestaurantMenuCard from './RestaurantMenuCard';
 
 const RestaurantMenu = () => {
     const {resId} = useParams();
     const resInfo = useRestaurantMenu(resId);
+    console.log(resInfo);
 
     return resInfo?.length===0 ? <Shimmer/> : (
-        resInfo.map(res => {
-            const {id, isVeg, name, defaultPrice, description, ratings} = res.card.info;
-            return (
-                <div className="menu" key={id}>
-                    <span>{isVeg ? '🟢' : '🔴'}</span>
-                    <h5>{name}</h5>
-                    <h6>₹ {defaultPrice/100}</h6>
-                    <p>{ratings?.aggregatedRating?.rating} ({ratings?.aggregatedRating?.ratingCountV2})</p>
-                    <h3>{description}</h3>
-                </div>
-            );
-        })
-    )
-}
+        <div className='flex flex-col gap-4 p-4'>
+            {resInfo?.map(({card}) => (
+                <RestaurantMenuCard key={card.card.categoryId} menu={card.card} />
+            ))}
+        </div>
+    );
+};
 
 export default RestaurantMenu;
---
+++ src/components/RestaurantMenuCard.js
@@ -0,0 +1,44 @@
+import {RESTAURANT_CDN_URL} from '../utils/constants';
+import { useState } from 'react';
+
+const RestaurantMenuCard = ({menu}) => {
+    const {title, itemCards} = menu;
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div className={`border-b-2 border-gray-300 pb-4 bg-amber-100 rounded-lg p-4 w-full m-4`}>
+            <div className='flex items-center justify-between cursor-pointer' onClick={() => setIsOpen(!isOpen)}>
+                <h3 className='text-2xl font-semibold'>{title} ({itemCards.length})</h3>
+                <span className='text-2xl cursor-pointer'>{isOpen ? '▲' : '▼'}</span>
+            </div>
+            <div className={`flex flex-col gap-4 ${isOpen ? 'block' : 'hidden'}`}>
+                {itemCards.map((item) => {
+                    const {id, itemAttribute, name, description, finalPrice, imageId} = item.card.info;
+                    return (
+                        <div key={id} className='flex items-center justify-between border-b-2 border-gray-300 pb-4 gap-4'>
+                            <div className='flex flex-col gap-2'>
+                                <span className='text-2xl'>{itemAttribute.vegClassifier === 'VEG' ? '🍃' : '🍗'}</span>
+                                <h3 className='text-lg font-semibold'>{name}</h3>
+                                <p className='text-sm text-gray-500'>₹ {finalPrice/100}</p>
+                                {
+                                    itemAttribute.ratings?.aggregatedRating?.ratingCountV2 && (
+                                        <div className='flex items-center gap-2'>
+                                            <span className='text-2xl text-yellow-500'>⭐ {itemAttribute.ratings.aggregatedRating.rating}</span>
+                                            <span className='text-2xl text-gray-500'>({itemAttribute.ratings.aggregatedRating.ratingCountV2})</span>
+                                        </div>
+                                    )
+                                }
+                                <p className='text-sm text-gray-500'>{description}</p>
+                            </div>
+                            <div className='w-48 h-48 flex flex-col items-center justify-center'>
+                                <img className='w-full h-full object-cover rounded-lg' src={RESTAURANT_CDN_URL+imageId} alt={name} />
+                                <button className='relative top-[-10px] bg-amber-500 text-white px-4 py-2 rounded-lg'>ADD <span className='absolute top-0 right-0'>+</span></button>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default RestaurantMenuCard;
